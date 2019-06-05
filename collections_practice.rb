74d1cd4afb83a81c1baa6bdbcc813a6dfb0c6625
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-05 20:05:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,17 +19,12 @@ def begins_with_r(tools)
 end
 
 def contains_a(array)
-#This will be the boolean answer that is returned at the end of the method.
-answer = nil
-matchfound = []
-array.each do |element|
-  if element.include?("a") == true || element.include?("a") == true
-    matchfound << element
+  matchfound = []
+  array.each do |element|
+    if element.include?("a") == true || element.include?("a") == true
+      matchfound << element
+    end
   end
-end
-#Logic looks a little funny, but its asking does the matchfound array include a "false" -- if that is true, then the answer is false, else the answer for #begins_with_r should be true.
-if matchfound.include?(false) == true
-  answer = false
-else
-  answer = true
+  #Logic looks a little funny, but its asking does the matchfound array include a "false" -- if that is true, then the answer is false, else the answer for #begins_with_r should be true.
+  matchfound
 end
