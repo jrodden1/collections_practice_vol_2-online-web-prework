badabd1bbb814caf3a46d56d11f2e3e11d62b975
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-05 20:01:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,9 +6,11 @@ def begins_with_r(tools)
   tools.each do |tool|
     if tool[0] == "r" || tool[0] == "R"
       matchfound << true
+    else
+      matchfound << false
     end
   end
-  #Logic looks a little funny, but its asking does the matchfound array include a "false" -- if that is true, then the answer is false, else the answer for #begins_with_r should be true. 
+  #Logic looks a little funny, but its asking does the matchfound array include a "false" -- if that is true, then the answer is false, else the answer for #begins_with_r should be true.
   if matchfound.include?(false) == true
     answer = false
   else
