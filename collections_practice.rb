e08391e4c3c35fe9d416ec62ca1699b0708029ca
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-05 19:59:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,16 @@
+require 'pry'
 def begins_with_r(tools)
   #This will be the boolean answer that is returned at the end of the method.
-  matchfound = false
+  answer = nil
+  matchfound = []
   tools.each do |tool|
     if tool[0] == "r" || tool[0] == "R"
-      matchfound = true
+      matchfound << true
     end
   end
-  matchfound
+  if matchfound.include?(false)
+    answer = false
+  else
+    answer = true
+  end
 end
